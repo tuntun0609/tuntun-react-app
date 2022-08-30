ea25d72b4cdd1f13611a0ab4b45a7ca439a97c9f
tuntun <56946732+tuntun0609@users.noreply.github.com>
2022-08-31 04:00:06 +0800

    chore: 优化控制台输出

--- config/webpack/webpack.dev.js
+++ config/webpack/webpack.dev.js
@@ -1,5 +1,7 @@
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // 模板
+const FriendlyErrorsWebpackPlugin = require('@soda/friendly-errors-webpack-plugin');
+const chalk = require('chalk');
 
 const common = require('./webpack.common.js');
 const { resolvePath } = require('../scripts/path.js');
@@ -16,17 +18,24 @@ module.exports = merge(common, {
 		// proxy: {
 		// 	'/api': 'http://localhost:8000',
 		// },
-		open: true,
+		// open: true,
 		compress: true,
 		hot: true,
 		port: 8000,
 	},
+	stats: 'none',
 	plugins: [
 		new HtmlWebpackPlugin({
 			title: 'tun react app',
 			template: resolvePath('../../public/index.html'),
 			filename: 'index.html',
 			favicon: resolvePath('../../public/favicon.ico'),
+		}),
+		new FriendlyErrorsWebpackPlugin({
+			compilationSuccessInfo: {
+				messages: [`You application is running here ${chalk.blue('http://localhost:8000')}`],
+				notes: [`To create a production build, use ${chalk.blue('npm run build')} or ${chalk.blue('pnpm build')}`],
+			},
 		}),
 	],
 	optimization: {
